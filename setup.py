35fb8320580362cbd283101494863ee75af1024e
Angel He <angel.heyongxi.2004@gmail.com>
2024-09-27 17:54:34 +0100

    added back find_packages in setup

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='my-housekeeper',  
     version='1.0',          
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'housekeeper=housekeeper:main', 
