6580fc91288e8d89069266a51814796766d1568a
Angel He <angel.heyongxi.2004@gmail.com>
2024-09-27 17:46:02 +0100

    removed `src` package ref in setup

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='my-housekeeper',  
     version='1.0',          
-    packages=find_packages(where='src'),  
-    package_dir={'': 'src'},              
     entry_points={
         'console_scripts': [
             'housekeeper=housekeeper:main', 
