ad6c5b301a3691b87a92bcaca37c270b54529aa0
Angel He <angel.heyongxi.2004@gmail.com>
2024-09-27 17:38:54 +0100

    rewrote installation setup

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='housekeeper',
-    version='1.0',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    install_requires=[
-        'tabulate',            
-        'google-generativeai',  
-    ],
+    name='my-housekeeper',  
+    version='1.0',          
+    packages=find_packages(where='src'),  
+    package_dir={'': 'src'},              
     entry_points={
         'console_scripts': [
-            'housekeeper=src.housekeeper:main',
+            'housekeeper=housekeeper:main', 
         ],
     },
+    install_requires=[
+        'tabulate',                      
+        'google-generativeai',
+    ],
+    classifiers=[                     
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',          
 )
